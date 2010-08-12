e715e7419b916d1709b500d74fbcbb4cf5841540
Doug MacEachern <dougm@vmware.com>
2010-08-12 15:12:55 -0700

    add windows::chef_client recipe

---
+++ windows/recipes/chef_client.rb
@@ -0,0 +1,47 @@
+#
+# Author:: Doug MacEachern <dougm@vmware.com>
+# Cookbook Name:: windows
+# Recipe:: chef_client
+#
+# Copyright 2010, VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#knife windows bootstrap fqdn -r windows::chef_client
+
+home = 'c:\chef'
+chef_exe = "#{home}\\chef-client.exe"
+service_name = "chefclient"
+
+directory home do
+  action :create
+end
+
+template "#{home}/chef-client.xml" do
+  source "chef-client.xml"
+end
+
+remote_file chef_exe do
+  source "http://maven.dyndns.org/2/com/sun/winsw/winsw/1.8/winsw-1.8-bin.exe"
+  not_if { File.exists?(chef_exe) }
+end
+
+execute "#{chef_exe} install" do
+  cwd home
+  only_if { WMI::Win32_Service.find(:first, :conditions => {:name => service_name}).nil? }
+end
+
+service service_name do
+  action :start
+end
