48cc7ad734c482613382975f06688122456368ab
Mat Schaffer <mat@schaffer.me>
2011-11-26 21:53:37 -0500

    Added windows dependency to fix open4 on ruby 1.9

--- windows/metadata.rb
+++ windows/metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Automate common windows tasks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.3"
+depends          "windows"
